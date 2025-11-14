54e43aa608700f6f736f4d74e8476b64ca87b812
Shivam <shivamc122@gmail.com>
2025-11-14 23:13:56 +0530

    This is code wriiten inside Git Demo need to refelect inside Git Stuff

--- src/basicAuto/TestTest12.java
+++ src/basicAuto/TestTest12.java
@@ -16,4 +16,18 @@ public static void main(String[] args) throws InterruptedException {
 		driver.get("https://www.facebook.com/?_rdr");   
 }
 
+
+public void Test2() {
+	
+	
+	//System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
+	
+			WebDriver driver=new ChromeDriver();
+			
+			driver.manage().window().maximize();   //To maximize Window
+			
+			driver.get("https://www.facebook.com/?_rdr");   
+	}
+
+
 }
