0afa9b0280aad23a7bdf88796240e5a22815a09c
Shivam <shivamc122@gmail.com>
2025-11-14 23:00:52 +0530

    GitStuff code Push

---
+++ src/basicAuto/TestTest12.java
@@ -0,0 +1,19 @@
+package basicAuto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestTest12 {
+
+public static void main(String[] args) throws InterruptedException {
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();   //To maximize Window
+		
+		driver.get("https://www.facebook.com/?_rdr");   
+}
+
+}
